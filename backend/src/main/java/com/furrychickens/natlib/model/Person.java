d9e004d0b51afaf0d535a5d5fb99db2657b75f0a
Rob <robmhanson@gmail.com>
2019-12-13 15:33:37 +1100

    Initial frontend and backend

---
+++ backend/src/main/java/com/furrychickens/natlib/model/Person.java
@@ -0,0 +1,22 @@
+package com.furrychickens.natlib.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Person {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NonNull
+    private String title;
+}
---
+++ backend/src/test/java/com/furrychickens/natlib/NatlibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.furrychickens.natlib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatlibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
